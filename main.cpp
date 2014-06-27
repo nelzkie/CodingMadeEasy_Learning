0f3974c710fceb8d7e97b51dabec0af3268b60d5
nelzkie <bellchan@Macintosh.local>
2014-06-28 00:35:30 +1200

    initial release

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CodingMadeEasy_learning
+//
+//  Created by Bellchan on 6/28/14.
+//  Copyright (c) 2014 Bellchan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
